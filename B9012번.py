2698fa39a17f4706eff0d1d8d06a94644376de7d
DHKim <dh95229505@gmail.com>
2024-10-24 22:43:13 +0900

    add: B9012번 성공

---
+++ B9012번.py
@@ -0,0 +1,32 @@
+# url:https://www.acmicpc.net/problem/9012
+# vps인지 아닌지 확인
+
+def is_vps(ps : str):
+    '''
+        parn_score : 여는 괄호일때 + 닫는 괄호일때 -
+
+    '''
+    parn_score =  0
+    for i in ps:
+        if parn_score >= 0:
+            if i == '(':
+                parn_score += 1
+            elif i == ')':
+                parn_score -= 1
+        else : break
+
+    if parn_score == 0:
+        return True
+    else :
+        return False
+
+T = int(input())
+a = []
+
+for i in range(T):
+    a.append(input())
+for i in range(T):
+    if is_vps(a[i]):
+        print("YES")
+    else:
+        print("NO")
