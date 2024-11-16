671da15c49e4aa920e4eae30ecf938fd165bc294
kimdonghyuk <144420387+hahagoodgood@users.noreply.github.com>
2024-11-17 00:05:26 +0900

    Add: B2579번:완료

--- B2579번.py
+++ B2579번.py
@@ -35,9 +35,12 @@ for i in range(n):
 if n == 1:
     print(inputs[1])
 else :
+    # 각 계단에서 얻을 수 있는 최고 점수를 기록한다.
     dp = [0]*(n+1)
     dp[1] = inputs[1]
     dp[2] = inputs[1]+inputs[2]
+
     for i in range(3, n+1):
         dp[i] = max(dp[i-2], dp[i-3] + inputs[i-1]) + inputs[i]
 
+    print(dp[n])
