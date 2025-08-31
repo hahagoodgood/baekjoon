544f8637c3ec83c2aa852df765e122d75e17ac93
Hyuk <dh95229505@gmail.com>
2025-08-31 21:24:28 +0900

    edit: 2747번 수정

--- B2747.py
+++ B2747.py
@@ -2,10 +2,8 @@ import sys
 
 def main(n:int):
     dp = [0]*(n+1)
-    dp[1] = 1
-    if n <= 1:
-        return sys.stdout.write(str(dp[n]))
-    else:
+    if n > 0:
+        dp[1] = 1
         for i in range(2, n+1):
             dp[i] = dp[i-1] + dp[i-2]
     return sys.stdout.write(str(dp[n]))
