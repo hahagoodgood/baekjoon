2627d1ae2f5631908f6499b0b5da19cdb5f57974
Hyuk <dh95229505@gmail.com>
2025-09-02 00:53:53 +0900

    No.1463 v2 Modified for efficient code

--- B1463.py
+++ B1463.py
@@ -20,30 +20,16 @@ Output the minimum number of operations required on the first line.
 '''
 import sys
 def B1453(n:int):
-    # n이 1일때
-    if n == 1:
-        return 0
-    # n이 3 이거나 2 일때
-    if n == 2 or n == 3:
-        return 1
-    #dp 테이블 초기화
-    dp = [107]*(n+1)
-    dp[0] = -1
-    dp[1] = 0
-    dp[2] = 1
-    dp[3] = 1
-    for i in range(4, n+1):
-        #2와 3으로 모두 나눠질 때
-        if not((i%2) or (i%3)):
-            dp[i] = min(dp[i-1], dp[i//2], dp[i//3]) + 1
-        # 3으로만 나눠질 때
-        elif not(i%3):
-            dp[i] = min(dp[i-1], dp[i//3]) + 1
-        # 2로만 나누질 때
-        elif not(i%2):
-            dp[i] = min(dp[i-1], dp[i//2]) + 1
-        else :
-            dp[i] = dp[i-1]+1
+    #DP table initialization
+    dp = [0]*(n+1)
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + 1
+        # When divisible only by 3
+        if i%3 == 0:
+            dp[i] = min(dp[i], dp[i//3]+1)
+        # When divisible only by 2
+        if i%2 == 0:
+            dp[i] = min(dp[i], dp[i//2]+1)
     return dp[n]
 
 if __name__ == '__main__':
