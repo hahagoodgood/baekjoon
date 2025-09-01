a96f0776c38c5e66c21c94f2a99656f796bfdd66
Hyuk <dh95229505@gmail.com>
2025-09-02 01:02:38 +0900

    No.2829 Annotated Translation

--- B2839.py
+++ B2839.py
@@ -1,33 +1,38 @@
 '''
-상근이는 요즘 설탕공장에서 설탕을 배달하고 있다. 상근이는 지금 사탕가게에 설탕을 정확하게 N킬로그램을 배달해야 한다. 설탕공장에서 만드는 설탕은 봉지에 담겨져 있다. 봉지는 3킬로그램 봉지와 5킬로그램 봉지가 있다.
+Sang-geun has been delivering sugar from the sugar factory lately.
+상Sang-geun must now deliver exactly N kilograms of sugar to the sweet shop.
+The sugar produced at the factory comes in bags.
+There are 3-kilogram bags and 5-kilogram bags.
 
-상근이는 귀찮기 때문에, 최대한 적은 봉지를 들고 가려고 한다. 예를 들어, 18킬로그램 설탕을 배달해야 할 때, 3킬로그램 봉지 6개를 가져가도 되지만, 5킬로그램 3개와 3킬로그램 1개를 배달하면, 더 적은 개수의 봉지를 배달할 수 있다.
+Sang-geun finds this troublesome and wants to carry as few bags as possible.
+For example, when he needs to deliver 18 kilograms of sugar,
+he could take 6 x 3-kilogram bags,
+but he could deliver it using fewer bags by taking 3 x 5-kilogram bags and 1 x 3-kilogram bag.
 
-상근이가 설탕을 정확하게 N킬로그램 배달해야 할 때, 봉지 몇 개를 가져가면 되는지 그 수를 구하는 프로그램을 작성하시오.
+Write a programme to determine the number of bags Sang-geun needs to take when he must deliver exactly N kilograms of sugar.
+---
+Input:
+The first line contains an integer N. (3 ≤ N ≤ 5000)
 
 ---
-입력:
-첫째 줄에 N이 주어진다. (3 ≤ N ≤ 5000)
-
----
-출력
-상근이가 배달하는 봉지의 최소 개수를 출력한다. 만약, 정확하게 N킬로그램을 만들 수 없다면 -1을 출력한다.
+Output:
+Output the minimum number of bags Sang-geun must deliver. If it is impossible to make exactly N kilograms, output -1.
 '''
 import sys
 def main(n:int):
 
-    #dp 테이블 초기화
+    #Initialising the DP table
     dp = [float('inf')]*((n//5)+1)
-    # 5키로 봉지 개수를 1개씩 증가
+    # Increase the number of 5kg bags by one each
     for i in range((n//5)+1):
-        # 5키로 봉지를 뺀 나머지가 3키로그람으로 나눠질 경우
+        # If the remainder after removing the 5-kilogramme bag is divided into 3-kilogramme portions,
         if (n-i*5)%3 == 0:
-            # i(5키로 봉지 개수) + 3키로 봉지 개수
+            # i(Number of 5kg bags) + Number of 3kg bags
             dp[i] = i+ (n-i*5)//3
-    # inf가 최소라면 -1 반환
+    # If inf is the minimum, return -1.
     if min(dp) == float('inf'):
         return -1
-    # dp로 탐색한 최소 갯수 출력
+    # Output the minimum number found by DP search
     return min(dp)
 
 if __name__ == '__main__':
