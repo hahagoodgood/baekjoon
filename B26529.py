c52ddd2ae2e613d5421f0d7fdc772e09b930bc38
Hyuk <dh95229505@gmail.com>
2025-09-09 23:58:37 +0900

    No.26529 v2 Solution using dynamic programming(Successful)

--- B26529.py
+++ B26529.py
@@ -42,11 +42,24 @@ F(0) = 1, F(1) = 1, F(N) = F(N-1)+F(N-2)
 '''
 import sys
 
+
+# def B26529(x:int):
+#     if x<=1:
+#         return 1
+#     else:
+#         return B26529(x-1) + B26529(x-2)
+
 def B26529(x:int):
     if x<=1:
         return 1
     else:
-        return B26529(x-1) + B26529(x-2)
+        dp = [0]*(x+1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, x+1): dp[i] = dp[i-1]+dp[i-2]
+        return dp[x]
+
+
 
 if __name__ == '__main__':
     n = int(sys.stdin.readline())
