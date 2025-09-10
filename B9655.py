55227d3640a4986bd901f66704436b78e1ff1d88
Hyuk <dh95229505@gmail.com>
2025-09-10 20:44:09 +0900

    No.9655 v 2
    Solution using conditions(Successful)

--- B9655.py
+++ B9655.py
@@ -38,21 +38,8 @@ import sys
 
 
 def B9655(N:int):
-    if N <= 3 :
-        if N%2 == 1 :return 'SK'
-        else: return 'CY'
-    else:
-        dp = ['']*(N+1)
-        dp[1] ='SK'
-        dp[2] = 'CY'
-        dp[3] = 'SK'
-        dp[4] = 'CY'
-        for i in range(5,N+1):
-            if dp[i-1] == 'CY' or dp[i-3] == 'CY':
-                dp[i] = 'SK'
-            else:
-                dp[i] = 'CY'
-        return dp[N]
+    if N%2==0:return 'CY'
+    return 'SK'
 
 if __name__ == '__main__':
     sys.stdout.write(B9655(int(sys.stdin.readline())))
